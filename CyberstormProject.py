76bd9dd6a1f356ce2b3a5ce64f5b0df5a23be76a
Hime0698 <jakesennett@me.com>
2017-04-24 17:02:43 -0500

    Lets get this party started!

--- CyberstormProject.py
+++ CyberstormProject.py
@@ -3,5 +3,23 @@
 #Date:
 #Description:
 ##############################################################################
+class game(object):
+    def __init__(self):
+        self.teams = {} # Holds team names and scores
+        self.questions1 = {} # load the picture with the questions and answers and the right answers
+        self.questions2 = {} # load the picture with the questions and answers and the right answers
+        self.questions3 = {} # load the picture with the questions and answers and the right answers
+        self.questions4 = {} # load the picture with the questions and answers and the right answers
 
-#The start of the project no code here yet
+    def setupGUI(self):
+        #set up the gui
+
+    def PlayGame(self):
+        #code for the game goes here
+
+    def intermission(self, teamname):
+        self.teams[teamname] = 0 #allows for setup of teams and initial points
+        
+
+    
+        
