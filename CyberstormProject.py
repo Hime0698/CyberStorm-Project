2b19703e0f43241783cfc75f6fff3c92c85f3acb
Hime0698 <jakesennett@me.com>
2017-04-27 15:42:41 -0500

    more updates

--- CyberstormProject.py
+++ CyberstormProject.py
@@ -1,5 +1,5 @@
 ###############################################################################
-#Names: Jacob Sennett, Nolan Lofton,
+#Names: Jacob Sennett, Nolan Lofton, Cotton Richardson, Tyler Nelson
 #Date:
 #Description:
 ##############################################################################
@@ -14,79 +14,78 @@ class game(object):
         self.questions2 = {} # load the picture with the questions and answers and the right answers
         self.questions3 = {} # load the picture with the questions and answers and the right answers
         self.questions4 = {} # load the picture with the questions and answers and the right answers
-        self.sounds = []
-        self.wronganswers = 3
         self.currentteam = "None"
-        self.a = "pin for a"
-        self.b = "pin for b"
-        self.c = "pin for c"
-        self.d = "pin for d"
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.IN) #placeholder pin number for a
+        GPIO.setup(16, GPIO.IN) #placeholder pin number for b
+        GPIO.setup(24, GPIO.IN) #placeholder pin number for c
+        GPIO.setup(12, GPIO.IN) #placeholder pin number for d
 
     def setupGUI(self):
+        pass
         #set up the gui
 
     def PlayGame(self):
-        while (self.wronganswers > 0)
+        wronganswers = 0
+        while (wronganswers < 3):
             "display the catagory screen"
             print "please select catagory"
-            if "gpio for catagory one" == self.a:
+            if GPIO.input(23): #subrutine for checking catagory one
                 currentquestion = random.choice(questions1.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions1[currentquestion]
+                if "GPIO input for answer" == questions1[currentquestion]:
                     "green led on"
-                    "play sound for right answer"
                     self.teams[self.currentteam] += 1
                 else:
                     "red LED on"
-                    "play wrong sound"
-                    self.wronganswers = self.wronganswers - 1
+                    wronganswers += 1
                     
-            elif "gpio for catagory one" == self.b:
+            elif GPIO.input(16):#subrutine for checking catagory two
                 currentquestion = random.choice(questions2.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions3[currentquestion]
+                if "GPIO input for answer" == questions3[currentquestion]:
                     "green led on"
-                    "play sound for right answer"
                     self.teams[self.currentteam] += 1
                 else:
                     "red LED on"
-                    "play wrong sound"
-                    self.wronganswers = self.wronganswers - 1
+                    wronganswers += 1
                     
-            elif "gpio for catagory one" == self.c:
+            elif GPIO.input(24):#subrutine for checking catagory three
                 currentquestion = random.choice(questions3.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions3[currentquestion]
+                if "GPIO input for answer" == questions3[currentquestion]:
                     "green led on"
-                    "play sound for right answer"
                     self.teams[self.currentteam] += 1
                 else:
                     "red LED on"
-                    "play wrong sound"
-                    self.wronganswers = self.wronganswers - 1
+                    wronganswers += 1
 
-            elif "gpio for catagory one" == self.d:
+            elif GPIO.input(12):#subrutine for checking catagory four
                 currentquestion = random.choice(questions4.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions4[currentquestion]
+                if "GPIO input for answer" == questions4[currentquestion]:
                     "green led on"
-                    "play sound for right answer"
                     self.teams[self.currentteam] += 1
                 else:
                     "red LED on"
-                    "play wrong sound"
-                    self.wronganswers = self.wronganswers - 1
+                    wronganswers += 1
 
             else:
                 print "Invalid Selection"
+        print "Game Over!"
+        intermission()
                 
             
         #code for the game goes here
 
-    def intermission(self, teamname):
+    def intermission(self):
         for key, value in teams.items():
-            print "{}: {}".format(key, value)
+            print "{}: {}/80".format(key, value)
+        print "type in team name in quotes"
         self.currentteam = input
         self.teams[self.currentteam] = 0 #allows for setup of teams and initial points
         #display the instructions picture
-        
+
+####################################################################################################################
+
+
