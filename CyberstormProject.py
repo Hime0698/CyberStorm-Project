f73ca67c91947c34cb11c6d983a48a0af2138242
Hime0698 <jakesennett@me.com>
2017-04-28 12:04:26 -0500

    updates as usual

--- CyberstormProject.py
+++ CyberstormProject.py
@@ -10,10 +10,10 @@ import pygame
 class game(object):
     def __init__(self):
         self.teams = {} # Holds team names and scores
-        self.questions1 = {} # load the picture with the questions and answers and the right answers
-        self.questions2 = {} # load the picture with the questions and answers and the right answers
-        self.questions3 = {} # load the picture with the questions and answers and the right answers
-        self.questions4 = {} # load the picture with the questions and answers and the right answers
+        self.questions1 = {} # load the picture with the questions and answers and the right answer
+        self.questions2 = {} # load the picture with the questions and answers and the right answer
+        self.questions3 = {} # load the picture with the questions and answers and the right answer
+        self.questions4 = {} # load the picture with the questions and answers and the right answer
         self.currentteam = "None"
         GPIO.setmode(GPIO.BCM) # sets up the GPIO pins
         GPIO.setup(23, GPIO.IN) # placeholder pin number for a
@@ -29,48 +29,56 @@ class game(object):
 
     def PlayGame(self):
         wronganswers = 0
-        while (wronganswers < 3): #continues to run through the catagories and selections as long as there are less than three wrong answers
+        while ((wronganswers < 3) and (len(self.questions1) + len(self.questions2) + len(self.questions3) + len(self.questions4) > 0)): #continues to run through the catagories and selections as long as there are less than three wrong answers
             GPIO.output(10, False)
             GPIO.output(11, True)
             "display the catagory screen"
             print "please select catagory"
             if GPIO.input(23): #subrutine for checking catagory one
-                currentquestion = random.choice(questions1.keys())
+                currentquestion = random.choice(self.questions1.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions1[currentquestion]:
+                if "GPIO input for answer" == self.questions1[currentquestion]:
+                    del self.questions1[currentquestion]
                     GPIO.output(10, True) # Green LED on
                     self.teams[self.currentteam] += 1
                 else:
+                    del self.questions1[currentquestion]
                     GPIO.output(11, True) # Red LED on
                     wronganswers += 1
                     
             elif GPIO.input(16):#subrutine for checking catagory two
-                currentquestion = random.choice(questions2.keys())
+                currentquestion = random.choice(self.questions2.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions3[currentquestion]:
+                if "GPIO input for answer" == self.questions2[currentquestion]:
+                    del self.questions2[currentquestion]
                     GPIO.output(10, True) # Green LED on
                     self.teams[self.currentteam] += 1
                 else:
+                    del self.questions2[currentquestion]
                     GPIO.output(11, True) # Red LED on
                     wronganswers += 1
                     
             elif GPIO.input(24):#subrutine for checking catagory three
-                currentquestion = random.choice(questions3.keys())
+                currentquestion = random.choice(self.questions3.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions3[currentquestion]:
+                if "GPIO input for answer" == self.questions3[currentquestion]:
+                    del self.questions3[currentquestion]
                     GPIO.output(10, True) # Green LED on
                     self.teams[self.currentteam] += 1
                 else:
+                    del self.questions3[currentquestion]
                     GPIO.output(11, True) # Red LED on
                     wronganswers += 1
 
             elif GPIO.input(12):#subrutine for checking catagory four
-                currentquestion = random.choice(questions4.keys())
+                currentquestion = random.choice(self.questions4.keys())
                 "display image stored in current question"
-                if "GPIO input for answer" == questions4[currentquestion]:
+                if "GPIO input for answer" == self.questions4[currentquestion]:
+                    del self.questions4[currentquestion]
                     GPIO.output(10, True) # Green LED on
                     self.teams[self.currentteam] += 1
                 else:
+                    del self.questions4[currentquestion]
                     GPIO.output(11, True) # Red LED on
                     wronganswers += 1
 
